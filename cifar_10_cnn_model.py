a4858885f6d953228631cf028e2506136b2bc303
will-sloan <willis5552@gmail.com>
2019-07-24 09:49:41 -0400

    2nd own cnn model that i'll be creating

---
+++ cifar_10_cnn_model.py
@@ -0,0 +1,5 @@
+'''
+Goal of this project is develop a cnn model to correctly identify cars, trucks, cat and dogs
+The reason for seperating out these types from all the others is to get practice manipulating datasets
+Once done the model, it will be used with a raspberry pi + camera to identify any visitors at our cottage
+'''
